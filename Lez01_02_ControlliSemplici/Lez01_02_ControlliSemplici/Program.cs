6376195b495ff224f48ab1635a5506d9dc966956
Giovanni Pace <g.pace@braintechinnovations.it>
2024-03-04 17:52:03 +0100

    Aggiunta IF

---
+++ Lez01_02_ControlliSemplici/Lez01_02_ControlliSemplici/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Lez01_02_ControlliSemplici
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * if(condizione){
+             *      //Codice in caso positivo
+             * } else {
+             *      //Codice in caso negativo
+             * }
+             */
+
+            int eta = 15;
+
+            if(eta >= 18)
+            {
+                Console.WriteLine("Sei maggiorenne");
+            } else
+            {
+                Console.WriteLine("Sei minorenne");
+            }
+        }
+    }
+}
