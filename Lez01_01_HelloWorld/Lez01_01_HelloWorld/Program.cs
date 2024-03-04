76714fb94ad07d2e61576b6fdf2b73c45a1a3218
Giovanni Pace <g.pace@braintechinnovations.it>
2024-03-04 17:32:21 +0100

    Prima lezione

---
+++ Lez01_01_HelloWorld/Lez01_01_HelloWorld/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Lez01_01_HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ciao, sono un commento a linea singola
+            /*
+             * Posso creare un commento
+             * multilinea
+             */
+            //Console.WriteLine("Hello, World!");         //Sono un commento alla fine della riga
+            //Console.WriteLine("Hello, World!");
+            //Console.WriteLine("Hello, World!");
+            //Console.WriteLine("Hello, World!");
+
+            //Ctrl + K + C Aggiunge un commento
+            //Ctrl + K + U Rimuove un commento
+
+            Console.WriteLine("Ciao Giovanni");
+            Console.WriteLine("Ciao Mario");
+            Console.WriteLine("Ciao Valeria");
+
+            Console.ReadLine();
+
+            Console.WriteLine("Ciao Marika");
+            Console.Write("Ciao");
+            Console.Write("Ciao");
+
+
+        }
+    }
+}
