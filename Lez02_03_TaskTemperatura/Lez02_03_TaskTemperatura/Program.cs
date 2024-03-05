b4f384437c9e5e980b9201fa3009c95e1ec2485d
Giovanni Pace <g.pace@braintechinnovations.it>
2024-03-05 15:07:33 +0100

    Aggiunta gestione Try Catch Finally

---
+++ Lez02_03_TaskTemperatura/Lez02_03_TaskTemperatura/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Lez02_03_TaskTemperatura
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Creare un sistema di controllo accessi al ristorante, in input (sotto forma di variabile) ci sarà la
+             * temperatura.
+             * 
+             * Se la temperatura è maggiore o uguale a 37.5° allora non permettere l'ingresso al ristorante.
+             * 
+             * ATTENZIONE: non ci vuole un medico per stabilire che al di sotto dei 35° abbiamo qualche problema o al di
+             * sopra dei 42 eviterei l'ingresso al ristorante.
+             */
+
+            float temp = 36.5f;
+
+            if (temp >= 35.0f && temp <= 42.0f)
+                if (temp < 37.5f)
+                    Console.WriteLine("Puoi entrare");
+                else
+                    Console.WriteLine("Non puoi entrare");
+            else
+                Console.WriteLine("Errore di validazione");
+        }
+    }
+}
---
+++ Lez02_04_InputUtente/Lez02_04_InputUtente/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Lez02_04_InputUtente
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Inserisci il tuo nominativo");
+            //string? input = Console.ReadLine();
+            //Console.WriteLine($"Il tuo nome è {input}");    
+
+            Console.WriteLine("Inserisci la tua età");
+            string? inputUtente = Console.ReadLine();
+
+            try
+            {
+                int valoreConvertito = Convert.ToInt32(inputUtente);
+
+                if (valoreConvertito >= 18)
+                    Console.WriteLine("Maggiorenne");
+                else
+                    Console.WriteLine("Minorenne");
+            } catch (FormatException ex)
+            {
+                Console.WriteLine($"Errore di formato");
+            } catch (Exception ex)
+            {
+                Console.WriteLine($"Errore generico");
+            } finally
+            {
+                Console.WriteLine("Programma terminato");
+            }
+
+
+        }
+    }
+}
