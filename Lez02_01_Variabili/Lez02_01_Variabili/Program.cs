a1366e21e39ac156e0513d70fb7935a9411b96de
Giovanni Pace <g.pace@braintechinnovations.it>
2024-03-05 14:20:04 +0100

    Aggiunta gestione stringhe

---
+++ Lez02_01_Variabili/Lez02_01_Variabili/Program.cs
@@ -0,0 +1,94 @@
+﻿namespace Lez02_01_Variabili
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Tipo di dato primitivo
+            //int eta = 15;
+            //int eta;            //Dichiarazione
+            //eta = 15;            //Assegnazione
+
+            //float b = 15.7f;
+            //Console.WriteLine(b);
+
+            //double c = 14.6d;
+            //Console.WriteLine(c);
+
+            //double d = b;
+            //Console.WriteLine(d);
+
+            //Console.WriteLine(b == d);
+
+            //-------------------------------------------
+
+            //int valoreUno = 8;                          //1
+            //int valoreDue = 5;                          //1
+
+            //Console.WriteLine(valoreUno + valoreDue);   //1
+            //Console.WriteLine(valoreUno + valoreDue);   //1
+            //Console.WriteLine(valoreUno + valoreDue);   //1
+            //Console.WriteLine(valoreUno + valoreDue);   //1
+
+            //-------------------------------------------
+
+            //int valoreUno = 8;                          //1
+            //int valoreDue = 5;                          //1
+            //int somma = valoreUno + valoreDue;          //2
+
+            //Console.WriteLine(somma);
+            //Console.WriteLine(somma);
+            //Console.WriteLine(somma);
+            //Console.WriteLine(somma);
+
+            //-------------------------------------------
+
+            //Operazione complessa
+            //Console.WriteLine( 15 + 16.9f + 23.5d );
+
+            //-------------------------------------------
+
+            //int numero = 5;
+            ////numero = numero + 1;
+            ////numero = numero + 4;
+
+            ////Console.WriteLine(numero);
+
+            //numero += 1;
+            //numero += 4;
+            //numero -= 2;
+
+            //Console.WriteLine(numero);
+
+            //-------------------------------------------
+
+            //int num = 0;
+
+            //num++;              // num += 1;            num = num + 1;
+            //num++;
+
+            //Console.WriteLine(num);
+
+            //num--;
+
+            //Console.WriteLine(num);
+
+            //-------------------------------------------
+
+            //int valUno = 15;
+            //float valDue = 2.3f;
+
+            //float rapporto = valUno / valDue;
+
+            //int risultato = (int)rapporto;
+
+            //Console.WriteLine(risultato);
+
+            //-------------------------------------------
+
+            int ciccio = 1;
+            int risultato = 2147483647 + ciccio;
+            Console.WriteLine(risultato);
+        }
+    }
+}
---
+++ Lez02_02_Stringhe/Lez02_02_Stringhe/Program.cs
@@ -0,0 +1,66 @@
+﻿namespace Lez02_02_Stringhe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string? nominativo = null;
+            //Console.WriteLine(nominativo);
+
+            //Console.WriteLine( "Giovanni " + "Pace" );
+
+            //int a = 2;
+            //int b = 5;
+
+            //Console.WriteLine("La somma dei numeri è: " + a + b);       //... è 25
+            //Console.WriteLine("La somma dei numeri è: " + (a + b));       //... è 7
+            //Console.WriteLine(a + b + " la somma dei numeri è");
+
+            //Operazioni semplici tra stringhe:
+            /*
+             * Scrivere un programma che mandi on output le seguenti due stringhe:
+             * 
+             * Giovanni Pace è 37 anni vecchio ed ha una temperatura corporea di 36.5°C.
+             * Mario Rossi è 23 anni vecchio ed ha una temperatura corporea di 38.5°C.
+             */
+
+            //string nominativo = "Giovanni Pace";
+            //int eta = 37;
+            //float temp = 36.5f;
+
+            //Console.WriteLine( $"{nominativo} è {eta} anni vecchio ed ha una temperatura di {temp}°C" );
+
+            //nominativo = "Mario Rossi";
+            //eta = 23;
+            //temp = 38.5f;
+
+            //Console.WriteLine( $"{nominativo} è {eta} anni vecchio ed ha una temperatura di {temp}°C" );
+
+            //--------------------------------------------------------------
+
+            //Console.WriteLine( $"Il risultato è: {5 + 6}" );
+
+            //string nominativo = "Giovanni Pace";
+            //Console.WriteLine($"Il nome {nominativo} ha una lunghezza di {nominativo.Length}");
+
+            //int lunghezza = nominativo.Length;
+            //Console.WriteLine($"Il nome {nominativo} ha una lunghezza di {lunghezza}");
+
+            //----------------------------------------------------
+
+            //string? frase = "Mi piace tanto \"programmare\"";
+            //Console.WriteLine(frase);
+
+            //----------------------------------------------------
+
+            String frase = "Sono Giovanni Pace e mi piace programmare";
+            //Console.WriteLine( frase.IndexOf("Pappagallo") );
+
+            if (frase.IndexOf("Pappagallo") != -1)
+                Console.WriteLine("Trovato");
+            else
+                Console.WriteLine("Non trovato");
+
+        }
+    }
+}
