33554bc97bd393ee745b991961924d37ca7d55c4
Giovanni Pace <g.pace@braintechinnovations.it>
2024-03-08 12:55:48 +0100

    Aggiunta gestione file

---
+++ Lez05_04_ProvaCSV/Lez05_04_ProvaCSV/Program.cs
@@ -0,0 +1,30 @@
+﻿using Lez05_04_ProvaCSV.Classes;
+
+namespace Lez05_04_ProvaCSV
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona gio = new Persona("Giovanni", "Pace", "PCAGNN");
+            Persona val = new Persona("Valeria", "Verdi", "VLRVRD");
+            Persona mar = new Persona("Marika", "Mariko", "MRKMRK");
+
+            string path = "C:\\Users\\ACADEMY\\Desktop\\persone.txt";
+
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(path))
+                {
+                    sw.WriteLine(gio.esportaCsv());
+                    sw.WriteLine(val.esportaCsv());
+                    sw.WriteLine(mar.esportaCsv());
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
