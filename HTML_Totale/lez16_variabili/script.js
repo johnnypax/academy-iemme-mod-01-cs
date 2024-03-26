7a784d4f08e3130acc64a06d71aec28db926df23
Giovanni Pace <g.pace@braintechinnovations.it>
2024-03-26 17:49:41 +0100

    Aggiunta gestione JS

---
+++ HTML_Totale/lez16_variabili/script.js
@@ -0,0 +1,14 @@
+// console.log("CIAO GIO");
+// console.log("CIAO GIO");
+// console.log("CIAO GIO");
+// console.log("CIAO GIO");
+// console.log("CIAO GIO");
+// console.log("CIAO GIO");
+
+var a = 5;
+var b = 6;
+
+if(a > b)
+    console.log("a maggiore di b")
+else
+    console.log("b maggiore di a")
